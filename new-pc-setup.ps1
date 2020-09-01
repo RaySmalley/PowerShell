7f0863b26e274d6a6ee84c1f0e4eb5c3df73d336
Ray Smalley <raysmalley@hotmail.com>
2020-09-01 15:26:16 -0400

    Update new-pc-setup.ps1

--- new-pc-setup.ps1
+++ new-pc-setup.ps1
@@ -15,6 +15,10 @@ Write-Host '# PC Setup Script           #'
 Write-Host '# Ray Smalley               #'
 Write-Host '# 2020                      #'`n
 
+# Download and extract Windows 10 Configuration Designer setup files
+Invoke-WebRequest https://raw.githubusercontent.com/RaySmalley/PowerShell/master/new-pc-setup.ps1 -OutFile $env:TEMP\new-pc-setup.zip
+Expand-Archive -Path $env:TEMP\new-pc-setup.zip -DestinationPath $PSScriptRoot -Force
+
 # Disable UAC
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force | Out-Null
 
@@ -301,6 +305,8 @@ Remove-Item "$env:TEMP\ChromeSetup.exe" -Force -ErrorAction SilentlyContinue
 Remove-Item "$env:TEMP\AdobeReaderSetup.exe" -Force -ErrorAction SilentlyContinue
 Remove-Item "$env:TEMP\test.pdf" -Force -ErrorAction SilentlyContinue
 Remove-Item "$env:TEMP\appdefaults.xml" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:TEMP\new-pc-setup.zip" -Force -ErrorAction SilentlyContinue
+
 
 # Re-enable UAC
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 1 -Force | Out-Null
